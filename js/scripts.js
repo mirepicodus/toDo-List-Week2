d422da62ae150d0d21d714e7a707a8bb1e557101
Matt Waechter & Mark Rubenstein <student@epicodus.com>
2014-07-14 17:15:05 -0700

    fix for adding date

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
   $(document).ready(function() {
-    var now = new Date;
-    var formDate = now.getMonth() +'/'+ now.getDate() +'/'+ now.getFullYear();
-  $('h1').append(" "+ formDate);
+
   $('form#tasks').submit(function(event) {
     var taskList = {};
     taskList.description = $('input#task').val();
     $('input#task').val("");
     taskList.status = false;
     $('ul#incomplete').append("<li>" + taskList.description + "</li>");
-    $('ul#incomplete li').click(function() {
-      taskList.status = true;
+    $('ul#incomplete li').last().click(function() {
+      var now = new Date;
+      var formDate = now.getMonth() +'/'+ now.getDate() +'/'+ now.getFullYear();
       $(this).remove();
       $('ul#complete').append(this);
-
+      $('ul#complete').append("<li>" + formDate + "</li>");
     });
 
     event.preventDefault();
